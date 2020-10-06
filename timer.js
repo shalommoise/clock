a94c65b846875d12b4312b681e47aabfddd0a9da
Shalommoise <mshalom689@gmail.com>
2020-10-06 23:17:20 +0100

    by the hour

--- timer.js
+++ timer.js
@@ -1,13 +1,15 @@
-let secondCount = 180;
-let minCount = 180;
-let hourCount = 180;
+const sec = Date().split(" ")[4].split(":")[2];
+const min = Date().split(" ")[4].split(":")[1];
+const hr = Date().split(" ")[4].split(":")[2];
+let secondCount = sec * 6;
+let minCount = min * 6;
+let hourCount = hr * 15;
 setInterval(function moveSecond() {
   secondCount += 6;
   if (secondCount === 360) secondCount = 0;
   document.getElementsByClassName(
     "secondhand"
   )[0].style.transform = `rotate(${secondCount}deg)`;
-  console.log("sec", secondCount / 6, minCount / 6, hourCount / 6);
 }, 1000);
 
 setInterval(function moveMin() {
@@ -16,13 +18,15 @@ setInterval(function moveMin() {
   document.getElementsByClassName(
     "minutehand"
   )[0].style.transform = `rotate(${minCount}deg)`;
-  console.log("min", secondCount / 6, minCount / 6, hourCount / 6);
 }, 60000);
 setInterval(function MoveHr() {
-  hourCount += 6;
+  hourCount += 15;
   if (hourCount === 360) hourCount = 0;
   document.getElementsByClassName(
     "hourhand"
   )[0].style.transform = `rotate(${hourCount}deg)`;
-  console.log("Hour", secondCount / 6, minCount / 6, hourCount / 6);
 }, 3600000);
+console.log(Date());
+console.log(Date().split(" ")[4].split(":")[0]);
+console.log(Date().split(" ")[4].split(":")[1]);
+console.log(Date().split(" ")[4].split(":")[2]);
