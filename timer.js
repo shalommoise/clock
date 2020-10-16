0d06546649a50720becee810bc7652f871e5c637
Shalommoise <mshalom689@gmail.com>
2020-10-16 14:46:31 +0100

    background changes based on pm or am

--- timer.js
+++ timer.js
@@ -4,11 +4,14 @@ setInterval(function(){
 let sec = Date().split(" ")[4].split(":")[2];
 let min = Date().split(" ")[4].split(":")[1];
 let hr = Date().split(" ")[4].split(":")[0];
-
   document.getElementsByClassName("secondhand")[0].style.transform = `rotate(${sec*6}deg)`; 
   document.getElementsByClassName("minutehand")[0].style.transform = `rotate(${min*6}deg)`;
-  if(hr > 12) hr -=12;
+  if(hr > 12){
+    document.getElementsByClassName("Outerclock")[0].style.background ="orange";
+    hr -=12;};
   document.getElementsByClassName("hourhand")[0].style.transform = `rotate(${hr*30}deg)`;
   document.getElementsByClassName("Outerclock")[0].style.display ="block"
+  
+ 
 }, 1000);
 
